66751cd0b32ebc50dd10773e0329dce3c8eb1178
David Hardt <82522618+DavidDotCheck@users.noreply.github.com>
2023-10-28 15:39:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+# Author: {{project.author}} <{{project.email}}>
+from setuptools import setup, find_packages
+
+setup(
+    name='{{project.distribution_name}}',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "setuptools"
+    ],
+    python_requires='>=3.8',
+    author='{{project.author}}',
+    author_email='{{project.email}}',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
